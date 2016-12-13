91f78f727eb438bd1bd3b6d272167d8ea9a5f3d7
David <djiaquin@gilbertschools.net>
2016-12-13 12:54:36 -0700

    Update team2.py

--- team2.py
+++ team2.py
@@ -28,24 +28,7 @@ def move(my_history, their_history, my_score, their_score):
     
     return 'c'
 
-    
-def test_move(my_history, their_history, my_score, their_score, result):
-    '''calls move(my_history, their_history, my_score, their_score)
-    from this module. Prints error if return value != result.
-    Returns True or False, dpending on whether result was as expected.
-    '''
-    real_result = move(my_history, their_history, my_score, their_score)
-    if real_result == result:
-        return True
-    else:
-        print("move(" +
-            ", ".join(["'"+my_history+"'", "'"+their_history+"'",
-                       str(my_score), str(their_score)])+
-            ") returned " + "'" + real_result + "'" +
-            " and should have returned '" + result + "'")
-        return False
-
-if __name__ == '__main__':
+    if __name__ == '__main__':
      
     # Test 1: Betray on first move.
     if test_move(my_history='',
@@ -66,3 +49,20 @@ if __name__ == '__main__':
               my_score=0, 
               their_score=0,
               result='b')             
+    
+def test_move(my_history, their_history, my_score, their_score, result):
+    '''calls move(my_history, their_history, my_score, their_score)
+    from this module. Prints error if return value != result.
+    Returns True or False, dpending on whether result was as expected.
+    '''
+    real_result = move(my_history, their_history, my_score, their_score)
+    if real_result == result:
+        return True
+    else:
+        print("move(" +
+            ", ".join(["'"+my_history+"'", "'"+their_history+"'",
+                       str(my_score), str(their_score)])+
+            ") returned " + "'" + real_result + "'" +
+            " and should have returned '" + result + "'")
+        return False
+
